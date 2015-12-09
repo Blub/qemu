2ad712aa3f4b54d76f9981920edf0d37222eca4e
Wolfgang Bumiller <w.bumiller@proxmox.com>
2015-12-09 15:41:13 +0100

    backup: do not use bdrv_drain_all

--- blockdev.c
+++ blockdev.c
@@ -3054,9 +3054,6 @@ static void pvebackup_cancel(void *opaque)
         vma_writer_set_error(backup_state.vmaw, "backup cancelled");
     }
 
-    /* drain all i/o (awake jobs waiting for aio) */
-    bdrv_drain_all();
-
     GList *l = backup_state.di_list;
     while (l) {
         PVEBackupDevInfo *di = (PVEBackupDevInfo *)l->data;
@@ -3065,8 +3062,7 @@ static void pvebackup_cancel(void *opaque)
             BlockJob *job = di->bs->job;
             if (job) {
                 if (!di->completed) {
-                     block_job_cancel_sync(job);
-                     bdrv_drain_all(); /* drain all i/o (awake jobs waiting for aio) */
+                    block_job_cancel_sync(job);
                 }
             }
         }
