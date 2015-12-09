de27ffeb5f55764a51b435533311f1d627d26177
Wolfgang Bumiller <w.bumiller@proxmox.com>
2015-12-09 16:04:32 +0100

    internal snapshot async

--- migration/savevm.c
+++ migration/savevm.c
@@ -891,11 +891,11 @@ void qemu_savevm_state_header(QEMUFile *f)
 
 }
 
-void qemu_savevm_state_begin(QEMUFile *f,
+int qemu_savevm_state_begin(QEMUFile *f,
                              const MigrationParams *params)
 {
     SaveStateEntry *se;
-    int ret;
+    int ret = 0;
 
     trace_savevm_state_begin();
     QTAILQ_FOREACH(se, &savevm_state.handlers, entry) {
@@ -923,6 +923,7 @@ void qemu_savevm_state_begin(QEMUFile *f,
             break;
         }
     }
+    return ret;
 }
 
 /*
@@ -1026,7 +1027,7 @@ void qemu_savevm_state_complete_postcopy(QEMUFile *f)
     qemu_fflush(f);
 }
 
-void qemu_savevm_state_complete_precopy(QEMUFile *f, bool iterable_only)
+int qemu_savevm_state_complete_precopy(QEMUFile *f, bool iterable_only)
 {
     QJSON *vmdesc;
     int vmdesc_len;
@@ -1060,12 +1061,12 @@ void qemu_savevm_state_complete_precopy(QEMUFile *f, bool iterable_only)
         save_section_footer(f, se);
         if (ret < 0) {
             qemu_file_set_error(f, ret);
-            return;
+            return ret;
         }
     }
 
     if (iterable_only) {
-        return;
+        return ret;
     }
 
     vmdesc = qjson_new();
@@ -1113,6 +1114,7 @@ void qemu_savevm_state_complete_precopy(QEMUFile *f, bool iterable_only)
     object_unref(OBJECT(vmdesc));
 
     qemu_fflush(f);
+    return qemu_file_get_error(f);
 }
 
 /* Give an estimate of the amount left to be transferred,
